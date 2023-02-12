de0cb02654225152a5b750d11244cded6632b53a
david diaz <diazmorenodavid16@gmail.com>
2023-02-12 11:25:24 -0500

    activity controller

--- api/src/controllers/activityController.js
+++ api/src/controllers/activityController.js
@@ -84,69 +84,6 @@ const createActivity = async (req,res,next) => {
   }
 }
 
-// async function createActivity(req, res){
-
-//   let { name, difficulty, duration, season, countries } = req.body;  
-  
-//   try {
-  
-//   if(name && difficulty && duration && season && countries){
-  
-//     name = name.charAt(0).toUpperCase()+name.slice(1).toLowerCase();
-    
-//       const validAct = await Activity.findOne({                  //Compruebo si ya existe dicha actividad...
-//         where: {
-//           name,
-//           difficulty,
-//           duration,
-//           season
-//         },
-//         attributes: {exclude: ['updatedAt', 'createdAt']} 
-//       });
-    
-//       if (!validAct) {
-    
-//         const addAct = await Activity.create({                //Creo la actividad y luego busco su pais para conectarlos...
-//           name,
-//           difficulty,
-//           duration,
-//           season
-//         });
-        
-//         const matchCountry = await Country.findAll({
-//           where: {
-//             name: countries,
-//           }
-//         });
-    
-//         const result = await addAct.addCountries(matchCountry);
-    
-//         return res.status(200).send(result);
-//       };
-    
-    
-//       //Busco pais que coincida para conectar la actividad existente..
-    
-//       const countryMatch = await Country.findAll({
-//         where: {
-//           name: countries,
-//         },
-//       });
-     
-    
-//       const result = await validAct.addCountries(countryMatch);
-    
-//       return res.status(200).send(result);
-    
-      
-//     }
-//   } catch (error) {
-//       res.status(400).send('No info in Data Base');
-//     }
-//   };
-
-
-
 module.exports = {
     getActivities,
     createActivity
